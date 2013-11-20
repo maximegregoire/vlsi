7a1d391934328179170fd052b160fe631ce36f06
maxime.gregoire@mail.mcgill.ca <maxime.gregoire@mail.mcgill.ca>
2013-11-20 14:32:58 -0500

    dma wip

--- fpga/first_nios2_system/synthesis/dma_engine.vhd
+++ fpga/first_nios2_system/synthesis/dma_engine.vhd
@@ -39,6 +39,7 @@ entity dma_engine is
 	read_address	: in 	std_logic_vector(10 downto 0);
 	write_address	: out 	std_logic_vector(10 downto 0);
 	write_enable	: out 	std_logic;
+	read_enable		: in	std_logic;
     );
 
 end dma_engine;
@@ -47,47 +48,47 @@ architecture functional of dma_engine is
   
   --signals
   signal data_sig : std_logic_vector(31 downto 0);
-  signal read_address_sig	:	std_logic_vector(10 downto 0);
   signal address_sig : std_logic_vector(31 downto 0);
   signal readen_sig : std_logic;
   signal write_enable_sig : std_logic;
+  signal write_address_sig : std_logic_vector(10 downto 0);
  
   byteenable <= "11";
   
-  process (sclk, resetN, read_address)
+  process (sclk, resetN, read_address, read_enable)
   begin
-  
   if resetN = '0' then
 	readen_sig <= '0';
 	address_sig <= (others => '0');
   elsif sclk'event and sclk = '1' then
-  if waitrequest = '0' and DMAEN = '1' then
-	read_address_sig <= read_address;
-	if read_address_sig /= read_address then
-		address_sig(10 downto 0) <= read_address;
-		readen_sig <= '1';
-	end if;
+  if waitrequest = '0' and DMAEN = '1' and read_enable = '1' then
+	address_sig(31 downto 10) <= DMAFSTART(22 downto 1);
+	address_sig(9 downto 0) <= read_address(9 downto 0);
+	readen_sig <= '1';
   end if;
   end if;
-  
   end process;
   
-  process (clk, resetN, readen)
+  process (clk, resetN, readen_sig, readdata_valid)
   begin
 	readdata_sig <= (others => '0');
-	
 	if resetN = '0' then
 		data_sig <= (others => '0');;
 	  elsif sclk'event and sclk = '1' then
 	  if readen_sig = '1' and readdata_valid = '1' then
+		write_address_sig  <= address_sig(10 downto 0);
 		data_sig(15 downto 0) <= readdata;
 		readen_sig <= '0';
 		write_enable_sig <= '1';
 	  end if;
 	  end if;
-	
   end process;
+  
+	--PUSH SIGNALS TO OUTPUT
+	data <= data_sig;
+	address <= address_sig;
+	readen <= readen_sig;
+	write_enable <= write_enable_sig;
+	write_address <= write_address_sig;
 
-  
-  
 end functional;
